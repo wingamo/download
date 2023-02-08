0c75fd11da15230025ab7f94822849ca2fd524ac
wingamo <104763726+wingamo@users.noreply.github.com>
2023-02-08 19:56:14 +0800

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,20 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta name="description" content="OneDrive Vercel Index" />
+          <meta name="description" content="WinGamo ISO Downloads." />
+          <meta name="keywords" content="WinGamo, wingamo, wingamodevs, wgd, windows"/>
+          <meta name="copyright" content="Copyright © 2023 WinGamo Devs. All rights reserved." />
+          <meta name="author" content="Adolo" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://www.wingamo.ga" />
+          <meta property="og:title" content="WinGamo Downloads" />
+          <meta property="og:description" content="WinGamo ISO Downloads" />
+          <meta property="og:image" content="https://cdn.discordapp.com/attachments/861217887489753092/1072848122641924167/wingamodevs.png" />
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://www.wingamo.ga" />
+          <meta property="twitter:title" content="WinGamo Downloads" />
+          <meta property="twitter:description" content="WinGamo ISO Downloads" />
+          <meta property="twitter:image" content="https://cdn.discordapp.com/attachments/861217887489753092/1072848122641924167/wingamodevs.png" />
           <link rel="icon" href="/favicon.ico" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
